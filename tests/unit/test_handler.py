64b75d419a7a329c3d07ff333596f193a6d47ed1
Victor Barba Martin <victor.barba@navigaglobal.com>
2021-10-18 10:39:18 +0200

    enable unit test

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -2,21 +2,21 @@ import json
 
 import pytest
 
-from hello_world import app
-
+from api_birthday import app
+from datetime import datetime,timedelta
 
 @pytest.fixture()
-def apigw_event():
+def put_event():
     """ Generates API GW Event"""
-
+    tomorrow = datetime.now()+ timedelta(days=-1)
     return {
-        "body": '{ "test": "body"}',
+        "body": '{"dateOfBirth": "'+tomorrow.strftime('%Y-%m-%d')+'" }',
         "resource": "/{proxy+}",
         "requestContext": {
             "resourceId": "123456",
             "apiId": "1234567890",
             "resourcePath": "/{proxy+}",
-            "httpMethod": "POST",
+            "httpMethod": "PUT",
             "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
             "accountId": "123456789012",
             "identity": {
@@ -54,20 +54,82 @@ def apigw_event():
             "User-Agent": "Custom User Agent String",
             "CloudFront-Forwarded-Proto": "https",
             "Accept-Encoding": "gzip, deflate, sdch",
+            "Content-Type": "application/json",
         },
-        "pathParameters": {"proxy": "/examplepath"},
-        "httpMethod": "POST",
+        "pathParameters": {"proxy": "/hello/aaa"},
+        "httpMethod": "PUT",
         "stageVariables": {"baz": "qux"},
-        "path": "/examplepath",
+        "path": "/hello/aaa",
     }
 
 
-def test_lambda_handler(apigw_event, mocker):
+@pytest.fixture()
+def get_event():
+    """ Generates API GW Event"""
+    return {
+        "body": '',
+        "resource": "/{proxy+}",
+        "requestContext": {
+            "resourceId": "123456",
+            "apiId": "1234567890",
+            "resourcePath": "/{proxy+}",
+            "httpMethod": "GET",
+            "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
+            "accountId": "123456789012",
+            "identity": {
+                "apiKey": "",
+                "userArn": "",
+                "cognitoAuthenticationType": "",
+                "caller": "",
+                "userAgent": "Custom User Agent String",
+                "user": "",
+                "cognitoIdentityPoolId": "",
+                "cognitoIdentityId": "",
+                "cognitoAuthenticationProvider": "",
+                "sourceIp": "127.0.0.1",
+                "accountId": "",
+            },
+            "stage": "prod",
+        },
+        "queryStringParameters": {"foo": "bar"},
+        "headers": {
+            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
+            "Accept-Language": "en-US,en;q=0.8",
+            "CloudFront-Is-Desktop-Viewer": "true",
+            "CloudFront-Is-SmartTV-Viewer": "false",
+            "CloudFront-Is-Mobile-Viewer": "false",
+            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
+            "CloudFront-Viewer-Country": "US",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Upgrade-Insecure-Requests": "1",
+            "X-Forwarded-Port": "443",
+            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
+            "X-Forwarded-Proto": "https",
+            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
+            "CloudFront-Is-Tablet-Viewer": "false",
+            "Cache-Control": "max-age=0",
+            "User-Agent": "Custom User Agent String",
+            "CloudFront-Forwarded-Proto": "https",
+            "Accept-Encoding": "gzip, deflate, sdch",
+            "Content-Type": "application/json",
+        },
+        "pathParameters": {"proxy": "/hello/aaa"},
+        "httpMethod": "GET",
+        "stageVariables": {"baz": "qux"},
+        "path": "/hello/aaa",
+    }
 
-    ret = app.lambda_handler(apigw_event, "")
+
+def test_put(put_event, mocker):
+
+    ret = app.app(put_event, "")
+    assert ret["statusCode"] == 200
+
+def test_get(get_event, mocker):
+
+    ret = app.app(get_event, "")
     data = json.loads(ret["body"])
 
     assert ret["statusCode"] == 200
-    assert "message" in ret["body"]
-    assert data["message"] == "hello world"
-    # assert "location" in data.dict_keys()
+    assert "message" in data
+    assert data["message"] == "Hello, aaa! Your birthday is in 364 day(s)"
